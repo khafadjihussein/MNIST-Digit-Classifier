4ad1a3c3bf7614fc7d2861223e18415876081711
khafadjihussein <khafadjihussein@gmail.com>
2025-09-18 21:58:00 +0100

    Update .gitignore and debug image save

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from infer import Predictor
 from PIL import Image, UnidentifiedImageError
 import io
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MNIST Digit Classifier API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:8080"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Prediction(BaseModel):
     digit: int
@@ -27,6 +39,7 @@ async def predict(file: UploadFile = File(...)):
     try:
         content = await file.read()
         img = Image.open(io.BytesIO(content))
+        img.save("debug_input.png")  # Save uploaded image for debugging
     except (UnidentifiedImageError, Exception):
         raise HTTPException(status_code=400, detail="Invalid image file.")
     pred = get_predictor()
